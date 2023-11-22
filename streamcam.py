53ac3d1eda3150696b23fed92230347cab6dda19
dnshshkr <danish.shukor@live.iium.edu.my>
2023-11-22 15:32:00 +0800

    modified:   config.ini 	modified:   streamcam.py

--- streamcam.py
+++ streamcam.py
@@ -30,8 +30,8 @@ if sys_platform=='linux':
     ip=get_ip.get_ip_linux('eth0')
 elif sys_platform=='windows':
     net_interface='Ethernet'
-    #ip=get_ip.get_ip_windows()
-    ip='10.0.3.147'
+    ip=get_ip.get_ip_windows(net_interface)
+    #ip='10.0.3.147'
 del sys_platform,platform,get_ip
 port=2608
 def camera_init():
@@ -119,56 +119,12 @@ def index():
     return 'Basler'
 def gen():
     while True:
-        # frame_count=0
-        # fps=0
-        # start=time.time()
-        # grabResult=camera.RetrieveResult(5000,py.TimeoutHandling_ThrowException)
-        # if grabResult.GrabSucceeded():
-        #     #image=cv2.cvtColor(grabResult.Array,cv2.COLOR_RGB2BGR)
-        #     image=grabResult.Array
-        #     frame_count+=1
-        #     stop=time.time()
-        #     duration=stop-start
-        #     if duration>=1.0:
-        #         fps=f'{frame_count/duration:.1f}'
-        #         frame_count=0
-        #         start=time.time()
-        #     cv2.putText(image,str(fps),(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-        #     if show_img:
-        #         cv2.imshow('frame',image)
-        #         cv2.waitKey(1)
-        # grabResult.Release()
         jpeg=cv2.imencode('.jpg',image,[int(cv2.IMWRITE_JPEG_QUALITY),img_quality])[1]
         frame=jpeg.tobytes()
         yield(b'--frame\r\n'
               b'Content-Type:image/jpeg\r\n'
               b'Content-Length: '+f"{len(frame)}".encode()+b'\r\n'
               b'\r\n'+frame+b'\r\n')
-        #grabResult.Release()
-
-
-        # grabResult=camera.RetrieveResult(4000,py.TimeoutHandling_ThrowException)
-        # if grabResult.GrabSucceeded():
-        #     #image=converter.Convert(grabResult).GetArray()
-        #     image=grabResult.Array
-        #     jpeg=cv2.imencode('.jpg',image)[1]
-        #     cv2.putText(jpeg,str(int(1/(time.time()-start))),(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-        #     cv2.imshow('frame',jpeg)
-        #     frame=jpeg.tobytes()
-        #     yield(b'--frame\r\n'
-        #           b'Content-Type:image/jpeg\r\n'
-        #           b'Content-Length: '+f"{len(frame)}".encode()+b'\r\n'
-        #           b'\r\n'+frame+b'\r\n')
-        # grabResult.Release()
-        # start=time.time()
-
-        # if grabResult.GrabSucceeded(): #without cv2
-        #     frame=grabResult.Array
-        #     frame=frame.tobytes()
-        #     yield(b'--frame\r\n'
-        #           b'Content-Type:image/jpeg'
-        #           b'\r\n'+frame+b'\r\n')
-        # grabResult.Release()
 @app.route('/stream')
 def stream():
     return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -196,6 +152,7 @@ if __name__=='__main__':
     #http_server.serve_forever()
     while True:
         cam_server_greenlet=gevent.spawn(cam_server.serve_forever)
+        print('Server is up')
         check_cable_greenlet=gevent.spawn(check_cable_periodically,cam_server)
         gevent.joinall([cam_server_greenlet,check_cable_greenlet])
         print('Checking cable status')
@@ -203,9 +160,8 @@ if __name__=='__main__':
         while True:
             if time.time()-start>=check_cable_interval:
                 if get_cable_status(net_interface):
-                    print('Cable plugged. Server is up')
+                    print('Cable plugged')
                     break
                 else:
-                    print('Cable unplugged')
+                    print('Cable unplugged',end='\r')
                     start=time.time()
-    #app.run(host='0.0.0.0',port=2608,threaded=True)
